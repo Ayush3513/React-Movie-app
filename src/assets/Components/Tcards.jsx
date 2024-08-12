e78f77740f4fa9b7ced1446eec779fa0ee212ac5
Ayush Chavda <user.email like : "ayushchavda@gmail.com>
2024-08-13 00:39:23 +0530

    change 3

--- src/assets/Components/Tcards.jsx
+++ src/assets/Components/Tcards.jsx
@@ -5,7 +5,7 @@ const Tcards = ({data,title}) => {
 
 
   return (
-    <div className='h-fit w-full flex flex-wrap gap-10 p-6  '>
+    <div className='h-fit w-full flex flex-wrap items-center justify-center gap-10 p-6  '>
            {data.map((e,i)=>{
             return <Link to={`/${e.media_type || title}/details/${e.id}`} key={i}  className="h-[40vh]  w-[14vw] mb-5 flex-shrink-0  relative">
             <img className='h-[90%] w-screen shadow-[10px_20px_40px_5px_rgb(0,0,0)] object-cover' src={`https://image.tmdb.org/t/p/original/${e.backdrop_path || e.poster_path || e.profile_path}`} alt="" />
